a7f4e01a6e5c4a363d37451ff96e348eec84770f
hoc0976267424 <thaihoc.mr.lucky@gmail.com>
2016-12-27 09:08:58 +0700

    -upload file

---
+++ UploadFile/upload2.php
@@ -0,0 +1,25 @@
+﻿<form name="frmupload" method="POST" action="" enctype="multipart/form-data">
+ <tr>
+  <td><input type="file" name="file"/></td>
+  <td><input type="submit" name="submit" value="upload" /></td>
+ </tr>
+</form>
+<?php
+ $link = "C:\wamp64\www\\";
+ if(isset($_POST['submit'])&&($_FILES['file']['tmp_name'])!=NULL)
+ {
+	 if($_FILES['file']['type']=="image/jpeg")
+	 {
+		 $link="C:\wamp64\www\\";
+		 $tmp_name=$_FILES['file']['tmp_name'];
+		 $name=$_FILES['file']['name'];
+		 $type=$_FILES['file']['type'];
+		 move_uploaded_file($tmp_name,$link.$name);
+		 echo "ten file :".$name." Kieu file :".$type." Duong Dan file :".$link;
+	 }
+	 else
+		 echo "file k hop le";
+ }		 
+?>
+
+		 
---
+++ UploadFile/upload3.php
@@ -0,0 +1,17 @@
+﻿<form name ="upload" method="POST" action="" enctype="multipart/form-data">
+<tr>
+  <td><input type="file" name="file"/></td>
+  <td><input type="submit" name="submit" value="upload" /></td>
+ </tr>
+</form>
+<?php
+  if(isset($_POST['submit']) && ($_FILES['file']['tmp_name'])!=NULL){
+	 if($_FILES['file']['type']=="image/jpeg"){
+        move_uploaded_file($_FILES['file']['tmp_name'] , 'C:\wamp64\www\\'.$_FILES['file']['name']);
+	 }
+	 else
+	 {
+		 echo "file khong hop le";
+	 }
+ }
+?>
